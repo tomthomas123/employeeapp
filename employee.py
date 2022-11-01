114ec64d52bd73724fecb27108a8bb9f3baa1213
tomthomas123 <tomthomas1248@gmail.com>
2022-11-01 18:33:00 +0530

    new branch added

--- employee.py
+++ employee.py
@@ -15,7 +15,19 @@ while True:
     choice = int(input("Enter your option : "))
     if choice==1:
         print("employee entry selected")
+        empcode =input("Enter the empcode  : ")
+        empname = input("enter the name : ")
         
+        designation = input("Enter the designation : ")
+        salary =input("Enter the salary : ")
+        cmpname = input("Enter the company name : ")
+        phone = input("Enter the phone number  : ")
+        emailid = input("enter the emailid  : ")
+        password = input("Enter the password : ")
+        sql = 'INSERT INTO `employee`(`empcode`, `empname`, `designation`, `salary`, `companyname`, `phone`, `emailid`, `password`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+        data = (empcode,empname,designation,salary,cmpname,phone,emailid,password)
+        mycursor.execute(sql,data)
+        mydatabase.commit()
     elif choice==2:
         print("view all employee selected ")
     elif choice==3:
